385e17c4da310c3aed53262c639c9888b7209e41
MatiasRocha92 <maty.comu@gmail.com>
2025-08-05 04:56:36 -0300

    Actualizar configuración de Vite para mejorar compatibilidad con WebSockets

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		host: true,
+		port: 5173,
+		strictPort: true,
+		hmr: {
+			port: 5173,
+			protocol: 'ws'
+		}
+	}
 })
